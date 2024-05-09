e12d08f3abbe7aa50f10db18aa80b3772800461f
Larissa Alves <lizalves.dg@gmail.com>
2024-05-09 01:36:09 -0300

    Att TiposVariaveis; Add operadores

---
+++ operadores/operadores.java
@@ -0,0 +1,25 @@
+public class operadores {
+   public static void main (String[] args) {
+      //classe Operadores.java
+      String nomeCompleto = "LINGUAGEM" + "JAVA";
+		System.out.println(nomeCompleto);
+      
+      //qual o resultado das expressoes abaixo?
+      String concatenacao ="?";
+      System.out.println(concatenacao);
+
+      concatenacao = 1+1+1+"1";
+      System.out.println(concatenacao);
+      
+      concatenacao = 1+"1"+1+1;
+      
+      concatenacao = 1+"1"+1+"1";
+      System.out.println(concatenacao);
+      
+      concatenacao = "1"+1+1+1;
+      System.out.println(concatenacao);
+      
+      concatenacao = "1"+(1+1+1);
+      System.out.println(concatenacao);
+   }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
+
+        
     }
 }
